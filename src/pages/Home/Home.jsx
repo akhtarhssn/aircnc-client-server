f3fcb75bf9cb4e037fc5cc565c8eeb51a5dbd415
Wixden <watchonlytutorial@gmail.com>
2023-06-28 07:08:14 +0600

    Firebase Sign in with eamail pass and google sign in complete. working on signup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,9 @@ import Categories from "../../components/Categories/Categories";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Categories />
-    </div>
+    </>
   );
 };
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -12,5 +14,13 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
   },
 ]);
